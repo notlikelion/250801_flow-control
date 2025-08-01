dd62579c2489791485ea4d660346a7a31fb968e1
notlikelion <notlikelion@gmail.com>
2025-08-01 10:03:57 +0900

    조건문 1단계

--- src/step3/Solution01.java
+++ src/step3/Solution01.java
@@ -12,5 +12,13 @@ public class Solution01 {
         // footSize는 이제 다시 '선언'
 //        System.out.println("당신의 발사이즈는 " + footSize + "입니다.");
         System.out.println("당신의 발사이즈는 %d mm 입니다.".formatted(footSize));
+        // 200mm 넘는 신발 x. 300mm 넘는 신발도 x.
+        if (footSize < 200) { // < 미만, > 초과, <= 이하, >= 이상.
+            System.out.println("200mm 미만입니다");
+        } // (조건) { 조건을 만족시켰을 때 실행될 코드(구문) }
+        if (footSize > 300) {
+            System.out.println("300mm 초과입니다");
+        }
+
     }
 }
