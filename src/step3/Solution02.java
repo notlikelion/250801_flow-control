c27060c71cd338892cd25734bad931a76cef337e
notlikelion <notlikelion@gmail.com>
2025-08-01 10:39:45 +0900

    switch - equals 이슈 해결

--- src/step3/Solution02.java
+++ src/step3/Solution02.java
@@ -10,12 +10,15 @@ public class Solution02 {
         // if 문으로 구현
         // size. S. M. L
         System.out.print("사이즈가 어떻게 되세요? (S, M, L) : ");
-        String size = sc.nextLine();
-        if (size == "S") {
+        String size = sc.nextLine(); // new String으로 새로 만들어진 값이라...
+//        if (size == "S") {
+        if (size.equals("S")) {
             System.out.println("사이즈가 작은 편이시네요.");
-        } else if (size == "M") {
+//        } else if (size == "M") {
+        } else if (size.equals("M")) {
             System.out.println("사이즈가 덜 작은 편이시네요.");
-        } else if (size == "L") {
+//        } else if (size == "L") {
+        } else if (size.equals("L")) {
             System.out.println("사이즈가 덜덜 작은 편이시네요.");
         } else {
             System.out.println("그런 사이즈 없습니다.");
