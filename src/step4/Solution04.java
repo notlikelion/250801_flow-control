a69ac739b47f58e68260a455d1f0d8286c88032b
notlikelion <notlikelion@gmail.com>
2025-08-01 13:07:34 +0900

    for 문

---
+++ src/step4/Solution04.java
@@ -0,0 +1,12 @@
+package step4;
+
+// for문, Array.
+public class Solution04 {
+    public static void main(String[] args) {
+        // for (초기식; 조건식; 증감식) { 반복할 코드 }
+        // 조건식이 true인 동안에 작동 (예도 여전히 true일 때 작동)
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
