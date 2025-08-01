890aa661ed022acaa17d97f01ffc4d9da4e55965
notlikelion <notlikelion@gmail.com>
2025-08-01 15:07:17 +0900

    for문 + 배열 (인덱스)

--- src/step4/Solution06.java
+++ src/step4/Solution06.java
@@ -1,12 +1,16 @@
 package step4;
 
+import java.util.Arrays;
+
 public class Solution06 {
     // 반복문 + 배열, forEach
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]); // 배열명[인덱스번호]
+            arr[i] += 10;
         }
+        System.out.println(Arrays.toString(arr));
         int sum = 0; // 반복되는 외부에 합을 처리할 변수를 두고,
         for (int i = 0; i < arr.length; i++) {
             sum += arr[i]; // 각각의 변수들을 합을 시킨 다음에
